3f2dd9f7ab7ae5f5b20378df7ace8a744809c1fe
AbdulManaf12 <sahitoa97@gmail.com>
2023-07-22 01:07:06 +0500

    File Caching issue resolved

--- app.py
+++ app.py
@@ -58,15 +58,56 @@ def predict():
             annotations = yolo_to_dict('static/runs/detect/predict/labels/input-image.txt', 'static/runs/detect/predict/input-image.png')
         else:
             annotations = ''
+        timestamp = now.strftime('%Y%m%d%H%M%S')
         return render_template('result.html', 
                             patient_name=patient_name, 
                             age=age, 
                             gender=gender,
                             side=side,projection=projection,
                             date_of_scan=date_of_scan, 
-                            annotations=annotations)
+                            annotations=annotations,
+                            timestamp=timestamp)
 
     return render_template('index.html')
+
+@app.route('/dicom_to_png_api_index')
+def dicom_to_png_api_index():
+    return render_template('dicom_to_png_api.html')
+
+@app.route("/dicom_to_png_api_start_converting", methods=['GET', 'POST'])
+def dicom_to_png_api_start_converting():        
+    if request.method == 'POST':
+        remove_files()
+        file = request.files.get('input-image')
+        if file:
+            file.save('static/dicom_to_png_api_convert-file.dicom')
+            dicom_to_png('static/dicom_to_png_api_convert-file.dicom', 'static/dicom_to_png_api_convert-file.png')
+            print('yes')
+    return render_template('dicom_to_png_api.html', file_path='static/dicom_to_png_api_convert-file.png')
+
+@app.route('/mobile_detection_api')
+def detection_api_index():
+    return render_template('android_detection_index.html')
+
+@app.route('/mobile_detection_api_predict', methods=['GET', 'POST'])
+def detection_api_predict():
+    remove_files()
+    if request.method == 'POST':
+        file = request.files.get('file')
+
+        if file:
+            filename = f"static/detection_api_input-image.png"
+            file.save(filename)
+            os.chdir('static/')
+            if not os.path.exists('API_DIR'):
+                os.mkdir('API_DIR')
+            os.chdir('API_DIR/')
+            model = YOLO('../YOLOv8x-best.pt')
+            model('../detection_api_input-image.png', save=True)
+            os.chdir('../../')
+
+        return render_template('android_detection_result.html')
+    return render_template('android_detection_index.html')
 
 def allowed_file(filename):
     ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
@@ -76,12 +117,20 @@ def allowed_file(filename):
 def remove_files():
     if os.path.isdir('static/runs/'):
         shutil.rmtree('static/runs')
+    if os.path.isdir('static/API_DIR/runs/'):
+        shutil.rmtree('static/API_DIR/runs')
     if os.path.exists('static/input-image.png'):
         os.remove('static/input-image.png')
     if os.path.exists('static/convert-file.dicom'):
         os.remove('static/convert-file.dicom')
     if os.path.exists('static/convert-file.png'):
         os.remove('static/convert-file.png')
+    if os.path.exists('static/dicom_to_png_api_convert-file.dicom'):
+        os.remove('static/dicom_to_png_api_convert-file.dicom')
+    if os.path.exists('static/dicom_to_png_api_convert-file.png'):
+        os.remove('static/dicom_to_png_api_convert-file.png')
+    if os.path.exists('static/detection_api_input-image.png'):
+        os.remove('static/detection_api_input-image.png')
 
 def dicom_to_png(input_file, output_file):
     im = pydicom.dcmread(input_file)
